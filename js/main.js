b344e0d51113673377f9e18e446bbbabc63e1659
anabescos <anabes88@gmail.com>
2021-01-28 10:32:54 +0100

    Add second event

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 const inputElement = document.querySelector ('.js-input');
 const buttonElement = document.querySelector ('.js-button');
 const hintElement = document.querySelector ('.paragraph1');
-const paragraph2Element = document.querySelector ('.paragraph2');
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max); 
@@ -10,11 +9,14 @@ function getRandomNumber(max) {
 const randomGenerator = getRandomNumber(100);
 console.log (randomGenerator);
 
-function generatedNumber () {
+// First event - input number - hints
+
+function generatedNumber (ev) {
+    ev.preventDefault()
     const inputValue = parseInt(inputElement.value);
     console.log (inputValue);
 
-    if (inputValue < 0 || inputValue > 100 || inputValue === ('')){
+    if (inputValue < 0 || inputValue > 100 || inputValue === '' || isNaN(inputValue)){
         hintElement.innerHTML = 'Pista: Escribe un número entre 0 y 100';
         console.log ('try again');
     } else if (inputValue < randomGenerator){
@@ -30,3 +32,20 @@ function generatedNumber () {
 } 
 
 buttonElement.addEventListener ('click', generatedNumber);
+
+// Second event - counter
+
+const paragraph2Element = document.querySelector ('.paragraph2');
+
+let acc = 0;
+
+function counter (){
+    if (hintElement.value === '¡Bien! ¡Has ganado, campeona!'){
+        acc += 0;
+    } else {
+        acc += 1;
+    }
+   paragraph2Element.innerHTML = `Número de intentos: ${acc}`; 
+}
+
+buttonElement.addEventListener ('click', counter);

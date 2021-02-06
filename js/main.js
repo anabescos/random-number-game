3eab04be03661575e9548abbbe33a40ea55bf01a
anabescos <anabes88@gmail.com>
2021-02-06 12:30:46 +0100

    Add indentation to html and remove debugger on JS script

--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
+'use strict';
 
 const inputElement = document.querySelector ('.js-input');
 const buttonElement = document.querySelector ('.js-button');
 const hintElement = document.querySelector ('.paragraph1');
+const randomGenerator = getRandomNumber(100);
+const paragraph2Element = document.querySelector ('.paragraph2');
 
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max); 
 }
 
-const randomGenerator = getRandomNumber(100);
-
-console.log (randomGenerator);
 
 // First event - input number - hints
 
-function generatedNumber (ev) {
+function generateNumber (ev) {
     ev.preventDefault()
     const inputValue = parseInt(inputElement.value);
-    console.log (inputValue);
+    
 
     if (inputValue < 0 || inputValue > 100 || inputValue === '' || isNaN(inputValue)){
         hintElement.innerHTML = 'Pista: Escribe un número entre 0 y 100';
-        console.log ('try again');
+        
     } else if (inputValue < randomGenerator){
         hintElement.innerHTML = 'Pista: El número introducido es demasiado bajo';
-        console.log ('too low!');
+        
     } else if (inputValue > randomGenerator) {
         hintElement.innerHTML = 'Pista: el número introducido es demasiado alto'
-        console.log ('too high!');
+        
     } else if (inputValue === randomGenerator){
         hintElement.innerHTML = '¡Bien! ¡Has ganado, campeona!'
-        console.log ('well done!');
+        
     }
 } 
 
-buttonElement.addEventListener ('click', generatedNumber);
+// buttonElement.addEventListener ('click', generateNumber);
 
 // Second event - counter
 
-const paragraph2Element = document.querySelector ('.paragraph2');
+
 
 let acc = 0;
 
@@ -52,4 +52,9 @@ function counter (){
    paragraph2Element.innerHTML = `Número de intentos: ${acc}`; 
 }
 
-buttonElement.addEventListener ('click', counter);
+function getButtonSetCounter() {
+    generateNumber();
+    counter();
+}
+
+buttonElement.addEventListener ('click', getButtonSetCounter);
